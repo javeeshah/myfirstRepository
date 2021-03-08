1f37be2bf29114d0e9c5e78c53ace2bc0d91efe2
Jawad <javee_shah@hotmail.com>
2021-03-09 02:18:12 +0400

    Added new About us View

--- GitDemoProject/Controllers/HomeController.cs
+++ GitDemoProject/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace GitDemoProject.Controllers
             return View();
         }
 
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
